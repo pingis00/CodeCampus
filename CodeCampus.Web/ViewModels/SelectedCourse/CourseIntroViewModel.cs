faae65088062d199c562ef560ff5d037224f8270
Andreas <pingis00@hotmail.com>
2024-04-29 10:42:57 +0200

    Desing for SingleCourseView finished

---
+++ CodeCampus.Web/ViewModels/SelectedCourse/CourseIntroViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CodeCampus.Web.Models.Components;
+
+namespace CodeCampus.Web.ViewModels.SelectedCourse;
+
+public class CourseIntroViewModel
+{
+    public string? Id { get; set; }
+    public ImageComponent BackgroundImage { get; set; } = null!;
+    public LinkComponent HomeLink { get; set; } = new LinkComponent();
+    public LinkComponent CoursesLink { get; set; } = new LinkComponent();
+    public string CourseView { get; set; } = null!;
+    public List<string> Tags { get; set; } = [];
+    public string CourseTitle { get; set; } = null!;
+    public string CourseDescription { get; set; } = null!;
+    public StarRatingComponent StarRating { get; set; } = new StarRatingComponent();
+    public string Reviews { get; set; } = null!;
+    public string Likes { get; set; } = null!;
+    public string CourseHours { get; set; } = null!;
+    public ImageComponent AuthorImage { get; set; } = new ImageComponent();
+    public string AuthorText { get; set; } = null!;
+    public string AuthorName { get; set; } = null!;
+}
---
+++ CodeCampus.Web/ViewModels/SelectedCourse/InstructorInfoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CodeCampus.Web.Models.Components;
+
+namespace CodeCampus.Web.ViewModels.SelectedCourse;
+
+public class InstructorInfoViewModel
+{
+    public ImageComponent InstructorImage { get; set; } = new ImageComponent();
+    public string Title { get; set; } = null!;
+    public string InstructorName { get; set; } = null!;
+    public string Biography { get; set; } = null!;
+    public LinkComponent YoutubeLink { get; set; } = new LinkComponent();
+    public LinkComponent FacebookLink { get; set; } = new LinkComponent();
+}
