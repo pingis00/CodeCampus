c192c97f6d319d03722d4cae130d9ceffbe0e19e
Andreas <pingis00@hotmail.com>
2024-05-01 15:37:57 +0200

    User registration and validation finished

--- CodeCampus.Web/Controllers/AuthController.cs
+++ CodeCampus.Web/Controllers/AuthController.cs
@@ -1,15 +1,56 @@
-﻿using CodeCampus.Web.ViewModels.Auth;
-using CodeCampus.Web.ViewModels.Contact;
+﻿using CodeCampus.Infrastructure.Entities;
+using CodeCampus.Web.ViewModels.Auth;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodeCampus.Web.Controllers;
 
-public class AuthController : Controller
+public class AuthController(UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager) : Controller
 {
+    private readonly UserManager<UserEntity> _userManager = userManager;
+    private readonly SignInManager<UserEntity> _signInManager = signInManager;
+
+    [HttpGet]
     [Route("/signup")]
     public IActionResult SignUp()
     {
-        var viewModel = new SignUpViewModel();
+        if (_signInManager.IsSignedIn(User))
+        {
+            return RedirectToAction("Details", "Account");
+        }
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/signup")]
+    public async Task<IActionResult> SignUp(SignUpViewModel viewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            var exists = await _userManager.Users.AnyAsync(x => x.Email == viewModel.Form.Email);
+            if (exists)
+            {
+                ModelState.AddModelError("AlreadyExists", "User with the same email address already exists");
+                ViewData["ErrorMessage"] = "User with the same email address already exists";
+                return View();
+            }
+
+            var userEntity = new UserEntity
+            {
+                FirstName = viewModel.Form.FirstName,
+                LastName = viewModel.Form.LastName,
+                Email = viewModel.Form.Email,
+                UserName = viewModel.Form.Email
+            };
+
+            var result = await _userManager.CreateAsync(userEntity, viewModel.Form.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("SignIn", "Auth");
+            }
+            
+        }
         return View(viewModel);
     }
 
---
+++ CodeCampus.Web/wwwroot/js/validation.js
@@ -0,0 +1,108 @@
+﻿const formErrorHandler = (element, validationResult) => {
+    let spanElement = document.querySelector(`[data-valmsg-for="${element.name}"]`)
+
+    if (validationResult) {
+        element.classList.remove('input-validation-error')
+        spanElement.classList.remove('field-validation-error')
+        spanElement.classList.add('field-validation-valid')
+        spanElement.innerHTML = ''
+    }
+    else {
+        element.classList.add('input-validation-error')
+        spanElement.classList.add('field-validation-error')
+        spanElement.classList.remove('field-validation-valid')
+        spanElement.innerHTML = element.dataset.valRequired
+    }
+}
+
+const textValidator = (element, minLength = 2) => {
+    if (element.value.length >= minLength) {
+        formErrorHandler(element, true)
+    }
+    else {
+        formErrorHandler(element, false)
+    }
+
+}
+
+const emailValidator = (element) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    formErrorHandler(element, regex.test(element.value))
+}
+
+const passwordValidator = (element) => {
+    if (element.dataset.valEqualtoOther !== undefined) {
+
+        let password = document.getElementsByName(element.dataset.valEqualtoOther.replace('*', 'Form'))[0].value
+
+        if (element.value === password)
+            formErrorHandler(element, true)
+        else
+            formErrorHandler(element, false)
+    }
+    else {
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+        formErrorHandler(element, regex.test(element.value))
+    }
+}
+
+const phoneValidator = (element) => {
+    const regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+    formErrorHandler(element, regex.test(element.value))
+}
+
+const postalcodeValidator = (element) => {
+    const regex = /^[0-9a-zA-Z\s]{3,10}$/
+    formErrorHandler(element, regex.test(element.value))
+}
+
+const checkboxValidator = (element) => {
+    if (element.checked) {
+        formErrorHandler(element, true)
+    }
+    else {
+        formErrorHandler(element, false)
+
+    }
+}
+
+let forms = document.querySelectorAll('form')
+let inputs = forms[0].querySelectorAll('input')
+
+inputs.forEach(input => {
+    if (input.dataset.val === 'true') {
+
+        if (input.type === 'checkbox') {
+            input.addEventListener('change', (e) => {
+                checkboxValidator(e.target)
+            })
+        }
+        else {
+            input.addEventListener('keyup', (e) => {
+
+                switch (e.target.type) {
+
+                    case 'text':
+                        textValidator(e.target)
+                        break;
+
+                    case 'email':
+                        emailValidator(e.target)
+                        break;
+
+                    case 'password':
+                        passwordValidator(e.target)
+                        break;
+
+                    case 'phone':
+                        phoneValidator(e.target)
+                        break;
+
+                    case 'postalcode':
+                        postalcodeValidator(e.target)
+                        break;
+                }
+            })
+        }
+    }
+})
