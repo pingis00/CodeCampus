af13c5f95aa78c79ccbaead1ce075c327da21234
Andreas <pingis00@hotmail.com>
2024-06-01 03:54:29 +0200

    Added a web-api and configured swagger

---
+++ CodeCampus_WebApi/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace CodeCampus_WebApi.Configurations;
+
+public static class SwaggerConfiguration
+{
+    public static void RegisterSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(x =>
+        {
+            x.SwaggerDoc("v1", new OpenApiInfo { Title = "Code Campus", Version = "v1" });
+
+            x.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Name = "X-Api-Key",
+                Description = "Enter API Key",
+            });
+
+            x.AddSecurityDefinition("AdminApiKey", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Name = "X-Admin-Api-Key",
+                Description = "Enter Admin API Key",
+            });
+
+            x.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "ApiKey"
+                        }
+                    },
+                    Array.Empty<string>()
+                },
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "AdminApiKey"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+    }
+}
